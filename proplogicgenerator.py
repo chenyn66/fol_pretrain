2bc2e46bffdddb6156337230ce71680ed4919d22
zachfurie <90987992+zachfurie@users.noreply.github.com>
2022-12-02 16:27:39 -0500

    Nodes less likely to generate children as layers increase

--- proplogicgenerator.py
+++ proplogicgenerator.py
@@ -3,23 +3,28 @@ import random
 
 
 class Node:
-    def __init__(self, name) -> None:
+    def __init__(self, name, layer) -> None:
         self.truth = random.choice([True, False])
         self.id = name
         self.children = []
         self.nots = []
         self.ops = []
+        self.layer = layer
         self.generate_children()
         
+        
     def generate_children(self):
-        num = random.randint(0, 2)
+        numlist = [1,1,1,1,2,2,2]
+        for l in range(0,self.layer):
+            numlist.append(0)
+        num = random.choice(numlist)
         if num >= 1:
-            self.children.append(Node(self.id * 2))
+            self.children.append(Node(self.id * 2, self.layer + 1))
             self.nots.append(random.choice([True, False]))
         if num >= 2:
-            self.children.append(Node(self.id * 2 + 1))
+            self.children.append(Node(self.id * 2 + 1, self.layer + 1))
             self.nots.append(random.choice([True, False]))
-            self.ops.append('and')
+            self.ops.append(random.choice(['and', 'or']))
             
             
     def evaluate(self):
@@ -36,6 +41,8 @@ class Node:
                 b = self.children[0].evaluate() == False
             if self.ops[0] == 'and':
                 self.truth =  a == b == True
+            elif self.ops[0] == 'or':
+                self.truth = a==True or b==True
         return self.truth
     
     def print_statements(self):
@@ -45,13 +52,14 @@ class Node:
         if len(self.children) == 0:
             print('{} = {}'.format(self.id, self.truth))
         elif len(self.children) == 1:
-            print('{} = {} -> {}'.format(self.children[0].id, self.nots[0], self.id))
+            print('if {} = {} then {} = True'.format(self.children[0].id, self.nots[0], self.id))
         elif len(self.children) == 2:
-            print('{} = {} {} {} = {} -> {}'.format(self.children[0].id, self.nots[0], self.ops[0], self.children[1].id, self.nots[1], self.id))
+            print('if {} = {} {} {} = {} then {} = True'.format(self.children[0].id, self.nots[0], self.ops[0], self.children[1].id, self.nots[1], self.id))
             
             
             
-root = Node(1)
+print("start")
+root = Node(1, 0)
 root.evaluate()
 root.print_statements()
 print("")
