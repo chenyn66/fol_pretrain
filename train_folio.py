7beab5c02ed7b233dad8fb1cada5a0b658a5451e
chenyn66 <chenyn66@icloud.com>
2022-12-15 05:08:22 -0500

    Update train_folio.py

--- train_folio.py
+++ train_folio.py
@@ -19,14 +19,21 @@ if __name__ == '__main__':
         for i in range(6):
             pretrain_data.append(data.SYLLO(t, num_samples=(i+1)*1000, depth=i+1))
 
+    pretrain_tester = []
+    for t in ['adj', 'noun']:
+        for i in range(6):
+            pretrain_tester.append(data.SYLLO(t, num_samples=(i+1)*100, depth=i+1))
+
     
     tokenizer = RobertaTokenizer.from_pretrained('roberta-large')
     pre_model = syllo_finetune.LMCLS('roberta-large')
 
     pretrain_data = torch.utils.data.ConcatDataset(pretrain_data)
+    pretrain_tester = torch.utils.data.ConcatDataset(pretrain_tester)
 
     train_loader = torch.utils.data.DataLoader(pretrain_data, batch_size=64, shuffle=True, collate_fn=syllo_finetune.collate_fn(tokenizer, True))
-    pre_model, acc = syllo_finetune.train(pre_model, train_loader, epoch=10, pbar=True, verbose=False)
+    test_loader = torch.utils.data.DataLoader(pretrain_tester, batch_size=16, shuffle=True, collate_fn=syllo_finetune.collate_fn(tokenizer, True))
+    pre_model, acc = syllo_finetune.train(pre_model, train_loader, test_loader=test_loader, epoch=10, pbar=True, verbose=False)
     print(f'Pretrain accuracy: {acc}')
 
 
